696b8c89776a244d918af19c5760e8fcc73611a1
Juan Carlos García Murillo <garciamjuancarlos14@gmail.com>
2023-05-10 15:26:32 -0600

    Adding home page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,21 @@
-import {Container} from "@mui/material";
+import {Container, TableContainer} from "@mui/material";
 import Header from "./Header"; 
 import Footer from "./Footer";
+import Link from "next/link";
 
 export default function Layout({children}) {
     return (
-        <>
-            <Header></Header>
+        // Since you only can return a single container 
+        // this technique helps to return a single element
+        // with its children. This approach doesn't render 
+        // this element so it keeps our code cleaner.
+        <> 
+            <Header title="Juan Carlos">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/contact">Contact</Link>
+                <Link href="/admin">Admin</Link>
+            </Header>
             <Container fixed>
                 <main>{children}</main>
             </Container>
